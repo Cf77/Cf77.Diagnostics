0431f00c650e59f8807b639c8f9094533db81411
Cf77 <catfe77@gmail.com>
2020-05-13 21:17:11 +0300

    test: private method and nullable types
    (cherry picked from commit ec1b91733eeb502187e047c09dbfeb4f60e563c0)

--- Cf77.Diagnostics.Test/AbpComplexTypedParamsAnalyzerTests.cs
+++ Cf77.Diagnostics.Test/AbpComplexTypedParamsAnalyzerTests.cs
@@ -56,6 +56,146 @@ namespace TestApp
             VerifyCSharpDiagnostic(test, expected);
         }
 
+        [TestMethod]
+        public void TwoComplexTypesInPrivateMethod()
+        {
+            var test = @"
+namespace Abp.Application.Services
+{
+    public interface IApplicationService
+    {
+    }
+}
+
+namespace TestApp
+{
+    class ComplexType
+    {
+
+    }
+
+    class SomeClass: Abp.Application.Services.IApplicationService
+    {
+        private void Method(string str, ComplexType p, ComplexType d)
+        {
+        }
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void NullableParameterTest()
+        {
+            var test = @"
+namespace Abp.Application.Services
+{
+    public interface IApplicationService
+    {
+    }
+}
+
+namespace TestApp
+{
+    class ComplexType
+    {
+
+    }
+
+    class SomeClass: Abp.Application.Services.IApplicationService
+    {
+        public void Method(ComplexType p, int? nullableParam)
+        {
+        }
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void TwoNullableComplexTypesTest()
+        {
+            var test = @"
+namespace Abp.Application.Services
+{
+    public interface IApplicationService
+    {
+    }
+}
+
+namespace TestApp
+{
+    struct ComplexType
+    {
+
+    }
+
+    class SomeClass: Abp.Application.Services.IApplicationService
+    {
+        public void Method(string str, ComplexType? p, ComplexType? d)
+        {
+        }
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = AbpComplexTypedParamsAnalyzer.DiagnosticId,
+                Message = string.Format("Abp service method '{0}' contains more than one complex-typed parameter", "Method"),
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 18, 21)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public void TwoComplexValueTypesTest()
+        {
+            var test = @"
+namespace Abp.Application.Services
+{
+    public interface IApplicationService
+    {
+    }
+}
+
+namespace TestApp
+{
+    struct ComplexType
+    {
+
+    }
+
+    class SomeClass: Abp.Application.Services.IApplicationService
+    {
+        public void Method(string str, ComplexType p, ComplexType d)
+        {
+        }
+    }
+}
+";
+            var expected = new DiagnosticResult
+            {
+                Id = AbpComplexTypedParamsAnalyzer.DiagnosticId,
+                Message = string.Format("Abp service method '{0}' contains more than one complex-typed parameter", "Method"),
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 18, 21)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
         {
             return new AbpComplexTypedParamsAnalyzer();
